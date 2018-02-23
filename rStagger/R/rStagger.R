4b6403bf72df4c041f3e2ca894099e5ff6182023
Arian Barakat <arianbarakat@Arian-Barakats-MacBook-Pro.local>
2018-02-23 14:17:08 +0100

    Added parallel functionality and fixed some other issues

--- rStagger/R/rStagger.R
+++ rStagger/R/rStagger.R
@@ -1,6 +1,6 @@
 #' @title callStagger
 #' 
-#' @description A function to call Stagger (NLP) from R
+#' @description A function to call Stagger (NLP) from R.  Works currently only on a UNIX system with Java installed.
 #' @import magrittr
 #' 
 #' @param textFile String or path to .txt file. If string, the string will be written to a temporary file that is deleted once the algorithm finishes
@@ -9,7 +9,7 @@
 #' @param modelFile Path to .bin modelfile (i.e. the Swedish file). Default NULL (uses included modelfile)
 #' @return Returns a dataframe with class c("data.frame", "rStagger")
 #' 
-#' @example \dontrun{callStagger(textFile = "Hej, mitt namn är Kung Julian)}
+#' @examples \donttest{callStagger(textFile = "Hej, mitt namn är Kung Julian)}
 #'
 #' @export
 callStagger <- function(textFile, outFile = NULL, jarFile = NULL, modelFile = NULL){
@@ -23,7 +23,7 @@ callStagger <- function(textFile, outFile = NULL, jarFile = NULL, modelFile = NU
     modelFile <- system.file("bin", "swedish.bin", package = "rStagger")
     
     if(identical(modelFile, "")){
-      unpackModelFile()
+      .unpackModelFile()
       modelFile <- system.file("bin", "swedish.bin", package = "rStagger")
     }
   }
@@ -48,36 +48,79 @@ callStagger <- function(textFile, outFile = NULL, jarFile = NULL, modelFile = NU
     write(callResult, file = outFile)
   }
   
-  return(stringToDF(callResult))
+  return(.stringToDF(callResult))
 }
 
-
-#' @title unpackModelFile
+#' @title callStagger.parallel
+#' 
+#' @description A function to call Stagger (NLP) from R in parallel.  Works currently only on a UNIX system with Java installed.
+#' 
+#' @param files A vector of textfiles (file names)
+#' @param file.dir Path to directory of textfiles
+#' @param out.dir Path to directory for output files
+#' @param jarFile Path to .jar (Stagger) file. Default NULL (uses included .jar file)
+#' @param modelFile Path to .bin modelfile (i.e. the Swedish file). Default NULL (uses included modelfile)
+#' @return Returns a .txt file containing the parsed text
+#' 
+#' @details Uses GNU Parallel. See more at \href{https://www.gnu.org/software/parallel/}{https://www.gnu.org/software/parallel/}
+#' 
+#' @examples \donttest{callStagger.parallel(files = c("test1.txt", "test2.txt"), file.dir = "~/Desktop/", "~/Desktop/")}
 #'
-#' @description Utility function to unpack modelfile (Swedish model)
-#' 
-#' @return Unpacked swedish.bin modelfile
-unpackModelFile <- function(){
-  file.bz2 <- system.file("bin","swedish.bin.bz2", package = "rStagger")
-  message("Unpacking swedish.bin.bz2 ...")
-  system(paste("bzip2 -d", file.bz2))
-  message("Done")
+#' @export
+callStagger.parallel <- function(files, file.dir, out.dir, jarFile = NULL, modelFile = NULL){
+  
+  if(is.null(jarFile)){
+    jarFile <- system.file("java", "stagger.jar", package = "rStagger")
+  }
+  
+  if(is.null(modelFile)){
+    modelFile <- system.file("bin", "swedish.bin", package = "rStagger")
+    
+    if(identical(modelFile, "")){
+      .unpackModelFile()
+      modelFile <- system.file("bin", "swedish.bin", package = "rStagger")
+    }
+  }
+  
+  dirs <- c(file.dir, out.dir)
+  
+  if(!all(dir.exists(dirs))){
+    stop(paste("Following directories does not exist(s): ", dirs[!dir.exists(dirs)]))  
+  }
+  
+  if(!all(file.exists(paste0(file.dir,files)))){
+    stop(paste("Following file(s) does not exist(s): ", files[!file.exists(paste0(file.dir,files))]))  
+  }
+  
+  callString <- paste0("parallel 'java -jar ", jarFile,
+                       " -modelfile ", modelFile,
+                       " -tag ", file.dir,
+                       "{} > ", out.dir , "staggerOut_{}' ::: ", paste(files, collapse = " "))
+  message("Running ...")
+  system(callString)
+  message("Done!")
+  
 }
 
-#' @title stringToDF
-#' @description Utility function
+#' @title readParsed
 #' 
-#' @param string A string file
+#' @description A function to read Stagger output into data.frame
+#' 
+#' @param file Path to file
 #' @param ... Other argument to pass to internal calls (read.delim())
+#' @return  Returns a dataframe with class c("data.frame", "rStagger")
+#' 
+#'
 #' @export
-stringToDF <- function(string, ...){
+readParsed <- function(file, ...){
   colnames <- c("Index", "Token", "Lemma","POS.coarse","POS.fine",
                 "Morphological.features", "Head", "Dependency", "Chunk.tag", "Chunk.type", 
                 "NE.tag", "NE.type","TokenID")
-  df <- read.delim(textConnection(string),col.names = colnames, header = FALSE, ...)
+  df <- read.delim(file,col.names = colnames, header = FALSE, ...)
   class(df) <- c(class(df), "rStagger")
   return(df)
 }
+
 
 #' @title extract_entity
 #'
@@ -112,4 +155,18 @@ extract_entity <- function(object, type = c("all","person","place","inst","produ
 }
 
 
+.unpackModelFile <- function(){
+  file.bz2 <- system.file("bin","swedish.bin.bz2", package = "rStagger")
+  message("Unpacking swedish.bin.bz2 ...")
+  system(paste("bzip2 -d", file.bz2))
+  message("Done")
+}
 
+.stringToDF <- function(string, ...){
+  colnames <- c("Index", "Token", "Lemma","POS.coarse","POS.fine",
+                "Morphological.features", "Head", "Dependency", "Chunk.tag", "Chunk.type", 
+                "NE.tag", "NE.type","TokenID")
+  df <- read.delim(textConnection(string),col.names = colnames, header = FALSE, ...)
+  class(df) <- c(class(df), "rStagger")
+  return(df)
+}
